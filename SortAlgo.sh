f8af1d2cc0c5a8aa402eb2dac7c81d92bd96ec5b
“dineshega” <dinesh15.ega@gmail.com>
2020-10-09 17:30:40 +0530

    Added: compute a+b*c

--- SortAlgo.sh
+++ SortAlgo.sh
@@ -5,3 +5,5 @@ echo " Sorting Arithmetic Computation Problem"
 read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
+
+x=$(awk 'BEGIN {print '$a'+('$b'*'$c')}')
