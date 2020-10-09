ef278396264cf6ee33bb208c58c283e92f46f27b
“dineshega” <dinesh15.ega@gmail.com>
2020-10-09 17:46:12 +0530

    Added: read values from dictionary into array

--- SortAlgo.sh
+++ SortAlgo.sh
@@ -4,6 +4,8 @@ echo " Sorting Arithmetic Computation Problem"
 
 declare -A result
 
+j=0
+
 read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
@@ -15,4 +17,11 @@ result[two]=$(awk 'BEGIN {print ('$a'*'$b')+'$c'}')
 
 
 result[three]=`echo "$y" | awk '{print ($0-int($0)<0.499)?int($0):int($0)+1}'`
-result[four]=`echo "$z" | awk '{print ($0-int($0)<0.499)?int($0):int($0)+1}'
+result[four]=`echo "$z" | awk '{print ($0-int($0)<0.499)?int($0):int($0)+1}'`
+
+for i in ${result[@]}
+do
+	res[$j]=$i
+	j=$((j+1))
+done
+echo ${res[@]}
