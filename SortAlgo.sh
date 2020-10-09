e7ee21ab121a9e052c3521c0ca336af9b9974dd5
“dineshega” <dinesh15.ega@gmail.com>
2020-10-09 17:36:02 +0530

    Added: compute c+a/b

--- SortAlgo.sh
+++ SortAlgo.sh
@@ -6,4 +6,6 @@ read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
 
-y=$(awk 'BEGIN {print ('$a'*'$b')+'$c'}')
+y=$(awk 'BEGIN {print '$c'+('$a'/'$b')}')
+
+y1=`echo "$y" | awk '{print ($0-int($0)<0.499)?int($0):int($0)+1}'`
