df20b50d23a8b668eb4ae48096b8a5939b3ca501
“dineshega” <dinesh15.ega@gmail.com>
2020-10-09 17:42:21 +0530

    Added: Stored result in Dictionary

--- SortAlgo.sh
+++ SortAlgo.sh
@@ -2,10 +2,17 @@
 
 echo " Sorting Arithmetic Computation Problem"
 
+declare -A result
+
 read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
 
+result[one]=$(awk 'BEGIN {print '$a'+('$b'*'$c')}')
+y=$(awk 'BEGIN {print '$c'+('$a'/'$b')}')
 z=$(awk 'BEGIN {print ('$a'%'$b')+'$c'}')
+result[two]=$(awk 'BEGIN {print ('$a'*'$b')+'$c'}')
 
-z1=`echo "$z" | awk '{print ($0-int($0)<0.499)?int($0):int($0)+1}'`
+
+result[three]=`echo "$y" | awk '{print ($0-int($0)<0.499)?int($0):int($0)+1}'`
+result[four]=`echo "$z" | awk '{print ($0-int($0)<0.499)?int($0):int($0)+1}'
